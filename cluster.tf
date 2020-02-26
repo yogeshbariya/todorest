afd6da6e196ead35cfff4a1cbb991f6cc3cc3f9f
Anil Bidari <bidari.anil@gmail.com>
2020-02-26 18:41:37 +0000

    cluster.tf created online with Bitbucket

---
+++ cluster.tf
@@ -0,0 +1,6 @@
+resource "google_container_cluster" "gke-cluster" {
+  name               = "my-first-gke-cluster"
+  network            = "default"
+  location           = "us-central1-a"
+  initial_node_count = 3
+}
