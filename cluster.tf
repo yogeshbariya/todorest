de603a728b1ba3f92ebdf682b3086f04ebcbdd03
Anil Bidari <bidari.anil@gmail.com>
2020-02-26 19:05:14 +0000

    cluster.tf edited online with Bitbucket

--- cluster.tf
+++ cluster.tf
@@ -1,5 +1,5 @@
 resource "google_container_cluster" "gke-cluster" {
-  name               = "my-first-gke-cluster1"
+  name               = "my-first-gke-cluster"
   network            = "default"
   location           = "us-central1-a"
   initial_node_count = 3
