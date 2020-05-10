1cd0e96d0f9684e6518ef293553b2758b11bfde7
shivaspk <shivaspk.siva@gmail.com>
2020-05-10 11:25:06 +0530

    test cases added

--- src/test/java/com/wf/ms/TodorestApplicationTests.java
+++ src/test/java/com/wf/ms/TodorestApplicationTests.java
@@ -1,13 +1,52 @@
 package com.wf.ms;
 
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+import java.util.Date;
+
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.MediaType;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.wf.ms.model.Todo;
 
 @SpringBootTest
+@AutoConfigureMockMvc
 class TodorestApplicationTests {
 
-	@Test
-	void contextLoads() {
-	}
+	@Autowired
+	private MockMvc mockMvc;
 
+	
+	@Test
+	public void getTodos() throws Exception
+	{	
+		mockMvc.perform(
+				MockMvcRequestBuilders.get("/todos"))
+		.andExpect(status().isOk());
+	}
+	
+	@Test
+	public void createTodo() throws Exception
+	{	
+		
+		Todo test = new Todo();
+		test.setDes("Testing now");
+		test.setDone(false);
+		test.setTargetDate(new Date());
+		test.setUser("Anil");
+		
+		ObjectMapper om = new ObjectMapper();
+		
+		mockMvc.perform(
+				MockMvcRequestBuilders.post("/todos")
+				.accept(MediaType.APPLICATION_JSON)
+				.content(om.writeValueAsString(test)))
+		.andExpect(status().isCreated());
+	}
 }
