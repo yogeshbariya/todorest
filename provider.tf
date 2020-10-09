b5b6e40eb0a584905cd00efe74b1b2382a71b993
Anil Bidari <bidari.anil@gmail.com>
2020-10-09 03:44:20 +0000

    provider.tf edited online with Bitbucket

--- provider.tf
+++ provider.tf
@@ -1,5 +1,5 @@
 provider "google" {
-  credentials = file("vocal-camera-264620-2257fe68d879.json")
+  credentials = file("sc.json")
   project     = "vocal-camera-264620"
   region      = "us-central1"
 }
